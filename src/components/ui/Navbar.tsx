cb2fb5cc20a81a61fd4dfe610c7b374d60b3f487
Alfredo Mora <Alfredo9712mt@gmail.com>
2023-11-22 10:48:36 -0800

    add logo

--- src/components/ui/Navbar.tsx
+++ src/components/ui/Navbar.tsx
@@ -4,13 +4,16 @@ import { getServerSession } from "next-auth";
 import React from "react";
 import Avatar from "./Avatar";
 import Auth from "./Auth";
+import Image from "next/image";
 
 const Navbar = async () => {
   const session = await getServerSession(authOptions);
 
   return (
     <nav className="flex justify-between h-14 items-center font-medium">
-      <p>Skill swap</p>
+      <div className="flex items-center gap-3">
+        <Image src={"/logo.png"} width={49} height={49} alt="logo" />
+      </div>
       <ul className="flex gap-2 items-center">
         <li>Home</li>
         {session?.user ? (
