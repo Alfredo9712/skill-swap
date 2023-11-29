054b5cfc05a032ec16d56400ec7ef62bb1535617
Alfredo Mora <Alfredo9712mt@gmail.com>
2023-11-28 20:53:04 -0800

    remove navbar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,10 +26,7 @@ export default function RootLayout({
       <NextAuthProvider>
         <body className={roboto.className}>
           <div className="flex justify-center sm:px-5 md:px-10 ">
-            <div className=" w-full md:max-w-5xl lg:max-w-7xl ">
-              <Navbar />
-              {children}
-            </div>
+            <div className=" w-full md:max-w-5xl lg:max-w-7xl ">{children}</div>
           </div>
         </body>
       </NextAuthProvider>
