6f072c9866f6ff8fb1cadbfb7282b2c41bdd23a5
Alfredo Mora <Alfredo9712mt@gmail.com>
2023-11-28 21:44:58 -0800

    update landing button positioning

--- src/components/content/Landing.tsx
+++ src/components/content/Landing.tsx
@@ -15,8 +15,8 @@ const Landing = async () => {
 
   return (
     <div className="flex justify-center flex-col items-center pt-10 sm:pt-10 md:px-10 md:pt-[10%] ">
-      <div className="flex items-center  w-full flex-col md:flex-row md:max-w-4xl lg:max-w-6xl">
-        <div className="text-5xl font-bold flex-initial flex flex-col gap-3">
+      <div className="flex items-center  w-full flex-col text-center md:text-left md:flex-row md:max-w-4xl lg:max-w-6xl">
+        <div className="text-5xl font-bold flex-initial flex flex-col gap-3 ">
           <h1>Welcome to Skill Swap</h1>
           <h2 className="text-3xl">
             Our platform isn{"'"}t just about skills; it{"'"}s about creating
@@ -27,7 +27,7 @@ const Landing = async () => {
             Join us in shaping the future, puzzle piece by puzzle piece.
           </h2>
           <div className="pt-5" />
-          <Auth className="max-w-xs text-2xl" />
+          <Auth className="w-48 text-2xl self-center md:self-start" />
         </div>
         <div className="flex-initial flex-shrink-0 relative w-[200px] h-[200px] md:w-[350px] md:h-[350px]  lg:w-[500px] lg:h-[500px]">
           <Image
