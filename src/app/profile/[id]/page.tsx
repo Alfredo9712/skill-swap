2ff7d9bfd700fb7b56e076fb8e1f38f8f3b86a1f
Alfredo Mora <Alfredo9712mt@gmail.com>
2024-01-15 21:35:35 -0800

    add basic logic to profile page

--- src/app/profile/[id]/page.tsx
+++ src/app/profile/[id]/page.tsx
@@ -1,7 +1,32 @@
 import React from "react";
+import { prisma } from "../../../../prisma/prismaClient";
+import { authOptions } from "@/app/api/auth/[...nextauth]/route";
+import { getServerSession } from "next-auth";
 
-const Profile = ({ params }: { params: { id: string } }) => {
-  return <div>User: {params.id}</div>;
+const Profile = async ({ params }: { params: { id: string } }) => {
+  const { user: sessionUser } = (await getServerSession(authOptions)) || {};
+
+  const user = await prisma.user.findUnique({
+    where: {
+      id: params.id,
+    },
+    include: {
+      profile: true,
+    },
+  });
+
+  if (!user) {
+    return <div>Uh oh, looks like there was no user found!</div>;
+  }
+  const { name } = user;
+  const isUsersProfile = sessionUser?.id === user.id;
+
+  return (
+    <div>
+      <h1>{`${isUsersProfile}`}</h1>
+      <h2>{name}</h2>
+    </div>
+  );
 };
 
 export default Profile;
