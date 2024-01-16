dadd2487fdab60201a6f8aed1f02a7121f520c5d
Alfredo Mora <Alfredo9712mt@gmail.com>
2024-01-15 21:07:29 -0800

    add link to logo

--- src/components/ui/Navbar.tsx
+++ src/components/ui/Navbar.tsx
@@ -2,6 +2,7 @@
 import React from "react";
 import Image from "next/image";
 import { usePathname } from "next/navigation";
+import Link from "next/link";
 
 import Avatar from "@/components/ui/Avatar";
 import { Session } from "next-auth";
@@ -17,7 +18,9 @@ const Navbar = ({ session }: NavbarProps) => {
   return (
     <nav className="flex justify-between h-14 items-center font-medium py-9">
       <div className="flex items-center gap-3">
-        <Image src={"/logo.png"} width={40} height={40} alt="logo" />
+        <Link href="/dashboard">
+          <Image src={"/logo.png"} width={40} height={40} alt="logo" />
+        </Link>
       </div>
       <ul className="flex gap-2 items-center">
         {user && user.image && (
